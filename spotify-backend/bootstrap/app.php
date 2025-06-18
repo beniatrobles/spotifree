0fe272c0f05afc60e4b2f173c7c0b5ea9ce88ad4
beniatrobles <b.roblesberasaluce@gmail.com>
2025-06-18 19:23:00 +0200

    sanctum instalado

--- spotify-backend/bootstrap/app.php
+++ spotify-backend/bootstrap/app.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,9 +15,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->group('api', [
+            CorsMiddleware::class,
+            EnsureFrontendRequestsAreStateful::class,
+            ThrottleRequests::class,
+            SubstituteBindings::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
